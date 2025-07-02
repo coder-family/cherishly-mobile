896defc3aafc24021649678641c0af79daf44c17
thaobuihb <thaohb@gmail.com>
2025-07-02 11:36:41 +0700

    fix configuration erro

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,16 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
-    plugins: ["@babel/plugin-transform-flow-strip-types"],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            'app/(tabs)': './app/(tabs)',
+          },
+        },
+      ],
+    ],
   };
 };
