f44df50b746e55612b8d5a9bdbf1eaff3165dd41
thaobuihb <thaohb@gmail.com>
2025-07-04 16:29:44 +0700

    moving the initializeAuth() dispatch outside of the component to run once when the app starts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,13 @@
 import { Slot } from 'expo-router';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { initializeAuth } from './redux/slices/authSlice';
 import { store } from './redux/store';
 
-export default function Layout() {
-  useEffect(() => {
-    // Initialize authentication on app start
-    store.dispatch(initializeAuth());
-  }, []);
+// Initialize authentication on app start
+store.dispatch(initializeAuth());
 
+export default function Layout() {
   return (
     <Provider store={store}>
       <Slot />
